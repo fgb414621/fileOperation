e707faa016b329b6e3afdc414a607889fb1d1287
fgb <1623323120@qq.com>
2023-12-04 16:24:02 +0800

    初始化工程

---
+++ fileOperation/src/cn/fbg/hello.java
@@ -0,0 +1,16 @@
+package cn.fbg;
+
+/**
+ * ClassName: hello
+ * Package: cn.fbg
+ * Description:
+ *
+ * @Author fgb
+ * @Create 2023/12/4 16:23
+ * @Version 1.0
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
